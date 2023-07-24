0d0a57f44ac22dbee2d7155700b634567e90e606
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-24 14:46:02 +0300

    changes in app tsx and listGroup

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Fragment } from "react";
 function App() {
   //items.map((item) => <li>{item}</li>); // we use {} to render data dynamically
   // here using items we can take each item and convert it an item of a different type
+  let items = ["New York", "Ethiopia", "Kenya", "England", "Canada"];
+  let heading = "Cities";
   return (
     <>
-      <ListGroup></ListGroup>
+      <ListGroup items={items} heading={heading}></ListGroup>
     </>
   );
 }
